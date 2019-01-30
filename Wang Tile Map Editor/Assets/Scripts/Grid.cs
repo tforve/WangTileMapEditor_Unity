047a619a810921e2ae479a4ad5bc5ca31e6f57b9
Forve <tobias.forve@gmx.de>
2019-01-30 15:40:20 +0100

    Fixed Array Error

--- Wang Tile Map Editor/Assets/Scripts/Grid.cs
+++ Wang Tile Map Editor/Assets/Scripts/Grid.cs
@@ -6,7 +6,7 @@ public class Grid : MonoBehaviour
 {
     // ----- Grid related
     private RectTransform rt;                               // RectTransform Component of Grid to get width and height
-    private float MAX_X, MAX_Y;                             // Max Tiles in height and width depending on screensize and tileSize
+    private int MAX_X, MAX_Y;                               // Max Tiles in height and width depending on screensize and tileSize
     [SerializeField] private Vector2 size;                  // Number of Tiles in X and Y direction. Should be later changeable for user
     private float tileSize = 32.0f;                         // Fix number from Sprite resolution. could be variable and get size from sprite himself maybe
 
@@ -15,7 +15,7 @@ public class Grid : MonoBehaviour
     private Tile tile;
 
     //Array of Tiles for weight
-    private Tile[][] tiles;
+    private Tile[,] tiles;
 
     //---------------------- Header END ----------------------
 
@@ -29,10 +29,14 @@ public class Grid : MonoBehaviour
     void GenerateGrid()
     {
         // has to be a multiple of 32*32 so that the tiles fit in
-        MAX_X = rt.rect.width  / tileSize;   //Screen.width / tileSize;
-        MAX_Y = rt.rect.height / tileSize;  //Screen.height / tileSize;
+        MAX_X = (int)(rt.rect.width  / tileSize);       //Screen.width / tileSize;
+        MAX_Y = (int)(rt.rect.height / tileSize);        //Screen.height / tileSize;
 
-        //for Debuging purpose 
+        //init tiles[][] 
+        tiles = new Tile[MAX_X,MAX_Y];
+
+
+        //for Debuging purpose only right now
         size.x = MAX_X;
         size.y = MAX_Y;
 
@@ -41,7 +45,7 @@ public class Grid : MonoBehaviour
             for (int y = 0; y < MAX_Y; y++)
             {
                 Tile tmp = Instantiate<Tile>(tile, new Vector3(x * tileSize, y * tileSize, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
-               // tiles[x][y] = tmp;
+                tiles[x,y] = tmp;
             }
         }
     }
