b3e32b1bcab915bb4b25ba5b88c5a27c860a4135
Forve <tobias.forve@gmx.de>
2019-01-30 18:56:31 +0100

    have to rework Grid system for Weight... dont get it

--- Wang Tile Map Editor/Assets/Scripts/Grid.cs
+++ Wang Tile Map Editor/Assets/Scripts/Grid.cs
@@ -6,16 +6,23 @@ public class Grid : MonoBehaviour
 {
     // ----- Grid related
     private RectTransform rt;                               // RectTransform Component of Grid to get width and height
-    private int MAX_X, MAX_Y;                               // Max Tiles in height and width depending on screensize and tileSize
+    private int MAX_X, MAX_Y;                               // Max index in height and width (its twice+1 Tiles) depending on screensize and tileSize.
+    private int MAX_Tiles_X, MAX_Tiles_Y;                   // amount of Tiles;
     [SerializeField] private Vector2 size;                  // Number of Tiles in X and Y direction. Should be later changeable for user
-    private float tileSize = 32.0f;                         // Fix number from Sprite resolution. could be variable and get size from sprite himself maybe
+    private float indexSize = 32.0f;                        // its indexSize = 32;
+    private float tileSize;                                 // Fix number from Sprite resolution indexSize * 2 +1. could be variable and get size from sprite himself maybe
+
 
     // ----- Tile related
     [SerializeField]
+    private Index indexTile;
+    [SerializeField]
     private Tile tile;
+    
 
     //Array of Tiles for weight
-    private Tile[,] tiles;
+    private Index[,] indexArray;
+    private Tile[,] tileArray;
 
     //---------------------- Header END ----------------------
 
@@ -28,26 +35,45 @@ public class Grid : MonoBehaviour
     ///<summary> Initialze the Grid with the correct width and height </summary>
     void GenerateGrid()
     {
-        // has to be a multiple of 32*32 so that the tiles fit in
-        MAX_X = (int)(rt.rect.width  / tileSize);       //Screen.width / tileSize;
-        MAX_Y = (int)(rt.rect.height / tileSize);        //Screen.height / tileSize;
+        // has to be a multiple of 32*32 so that the indexArray fit in
+        MAX_X = (int)(rt.rect.width / indexSize);
+        MAX_Y = (int)(rt.rect.height / indexSize);
 
-        //init tiles[][] 
-        tiles = new Tile[MAX_X,MAX_Y];
+        MAX_Tiles_X = (MAX_X / 2) - 1;
+        MAX_Tiles_Y = (MAX_Y / 2) - 1;
 
+        //init indexArray[][] and tileArray[][]
+        indexArray = new Index[MAX_X, MAX_Y];
+        tileArray = new Tile[MAX_Tiles_X, MAX_Tiles_Y];
 
         //for Debuging purpose only right now
         size.x = MAX_X;
         size.y = MAX_Y;
 
+
+        //Forloop to instantiate Tiles
+          for (int x = 0; x < 1; x++)
+        {
+            for (int y = 0; y < 1; y++)
+            {
+                //Index
+                Tile tileTmp = Instantiate<Tile>(tile, new Vector3(x * (indexSize*2) +1, y * (indexSize*2)+1, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                tileArray[x, y] = tileTmp;
+            }
+        }
+        
+        //Forloop to instantiate Index
         for (int x = 0; x < MAX_X; x++)
         {
             for (int y = 0; y < MAX_Y; y++)
             {
-                Tile tmp = Instantiate<Tile>(tile, new Vector3(x * tileSize, y * tileSize, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
-                tiles[x,y] = tmp;
+                //Index
+                Index indexTmp = Instantiate<Index>(indexTile, new Vector3(x * indexSize, y * indexSize, 0), Quaternion.identity, GameObject.FindGameObjectWithTag("Canvas").transform);
+                indexArray[x, y] = indexTmp;
             }
         }
+
+      
     }
 
     ///<summary> Reset Grid if you want to restart Level OR make new one etc </summary>
---
+++ Wang Tile Map Editor/Assets/Scripts/Index.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Index : MonoBehaviour
+{
+    private Image image;
+
+    public Image MyImage
+    {
+        get { return image; }
+        set { image = value; }
+    }
+}
